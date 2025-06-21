0c25b963d2d12e757f6a10eb9cf2a374362c3a0e
rsvrx <35167039+yaelahrip@users.noreply.github.com>
2025-06-22 06:40:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -133,5 +133,7 @@ def receive_checkout():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 
+
 if __name__ == '__main__':
-    app.run(port=5031, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
